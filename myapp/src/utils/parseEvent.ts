d0c95a9a0a08aeb3bf6a8dce6de5e5c91b4de79e
Mikhail Kultyiasau <mikhail.kultiyasov@tispr.com>
2025-07-06 14:43:10 +0200

    Add simple implementation of parseEvent event function

--- myapp/src/utils/parseEvent.ts
+++ myapp/src/utils/parseEvent.ts
@@ -2,5 +2,33 @@ import { Event } from "../model/Event";
 import { Mappings } from "../model/Mappings";
 
 export function parseEvent(str: string, mappings: Mappings): Event | null {
-    throw new Error("Not implemented yet");
+    const [
+        id,
+        sportId,
+        competitionId,
+        startTimeStr,
+        homeTeamId,
+        awayTeamId,
+        statusId,
+        scoresRaw,
+    ] = str.split(",");
+
+    return {
+        id,
+        sport: mappings[sportId],
+        competition: mappings[competitionId],
+        startTime: new Date(Number(startTimeStr)).toISOString(),
+        competitors: {
+            HOME: { type: "HOME", name: mappings[homeTeamId] },
+            AWAY: { type: "AWAY", name: mappings[awayTeamId] },
+        },
+        status: mappings[statusId],
+        scores: {
+            CURRENT: {
+                type: "CURRENT",
+                home: scoresRaw.split("@")[1].split(":")[0],
+                away: scoresRaw.split("@")[1].split(":")[1],
+            },
+        },
+    };
 }
