a191d5f2c8579cdf7b5a6c455bd98694a0286073
Mikhail Kultyiasau <mikhail.kultiyasov@tispr.com>
2025-07-06 13:48:11 +0200

    Implement parseMappings utility method

--- myapp/src/utils/parseMappings.ts
+++ myapp/src/utils/parseMappings.ts
@@ -1,3 +1,20 @@
-export function parseMappings(str: string): Record<string, string> {
-    throw new Error("Not implemented yet");
+import {Mappings} from "../model/Mappings";
+
+export function parseMappings(str: string): Mappings {
+    const mappings: Mappings = {};
+
+    if (!str.trim()) {
+        return mappings;
+    }
+
+    const pairs = str.split(";");
+
+    for (const pair of pairs) {
+        const [id, name] = pair.split(":");
+        if (id && name) {
+            mappings[id] = name;
+        }
+    }
+
+    return mappings;
 }
--- myapp/test/utils/parseMappings.test.ts
+++ myapp/test/utils/parseMappings.test.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect } from "vitest";
+import {Mappings} from "../../src/model/Mappings";
 import { parseMappings } from "../../src/utils/parseMappings";
 
 describe("Parse Mappings Tests", () => {
@@ -10,7 +11,7 @@ describe("Parse Mappings Tests", () => {
         const expected = {
             "29190088-763e-4d1c-861a-d16dbfcf858c": "Real Madrid",
             "33ff69aa-c714-470c-b90d-d3883c95adce": "Barcelona"
-        };
+        } as Mappings;
 
         expect(parseMappings(raw)).toEqual(expected);
     });
